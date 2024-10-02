23c7d2cccd6093635178c1de98a91efc95885772
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-10-02 23:42:11 +0300

    support .counterignore

--- git_logger.sh
+++ git_logger.sh
@@ -1,4 +1,45 @@
-if [[ "$1" == "add" || "$1" == "commit" || "$1" == "push" || "$1" == "status" ]]; then
+# if [[ "$1" == "add" || "$1" == "commit" || "$1" == "push" || "$1" == "status" ]]; then
+#     # Do not log these commands, just pass them to git
+#     git "$@"
+# else
+#     # Log the command (with date and time) and pass it to git
+#     git "$@"
+#     exit_status=$?
+
+#     # If the exit status is 0 (success), log the command
+#     #echo $exit_status
+#     if [ $exit_status -eq 0 ]; then
+#         echo "$(date) git $@" >> ~/gitcounter/frequency.log
+#     else
+#         echo "git-logger: The git command failed and will not be logged"
+#     fi
+#     #echo "$(date) git $@" >> ~/gitcounter/frequency.log
+    
+
+#     exit $exit_status
+# fi
+
+ignore_file="./.counterignore"
+
+if [[ -f "$ignore_file" ]]; then
+  # Read the ignored keywords into an array
+  IFS=$'\n' read -d '' -r -a ignored_keywords < "$ignore_file"
+else
+  # If the file doesn't exist, exit with an error
+  echo "Error: .counterignore file not found at $ignore_file"
+  exit 1
+fi
+
+is_ignored_command() {
+  for keyword in "${ignored_keywords[@]}"; do
+    if [[ "$1" == "$keyword" ]]; then
+      return 0  # Command is ignored
+    fi
+  done
+  return 1  # Command is not ignored
+}
+
+if is_ignored_command "$1"; then
     # Do not log these commands, just pass them to git
     git "$@"
 else
@@ -9,31 +50,12 @@ else
     # If the exit status is 0 (success), log the command
     #echo $exit_status
     if [ $exit_status -eq 0 ]; then
-        echo "$(date) git $@" >> ~/gitcounter/frequency.log
+        echo "$(date) git $@" >> ./frequency.log
     else
         echo "git-logger: The git command failed and will not be logged"
     fi
-    #echo "$(date) git $@" >> ~/gitcounter/frequency.log
+    
     
 
     exit $exit_status
 fi
-
-
-# # Check if the command is one of the ignored ones
-# if [[ "$1" == "add" || "$1" == "commit" || "$1" == "push" ]]; then
-#   # Directly execute the command without logging
-#   git "$@"
-# else
-#   # Execute the git command and capture the exit status
-#   git "$@"
-#   exit_status=$?
-
-#   # If the exit status is 0 (success), log the command
-#   if [ $exit_status -eq 0 ]; then
-#     echo "$(date) $@" >> ~/.git_command_log
-#   fi
-
-#   # Return the original exit status to the shell
-#   exit $exit_status
-# fi
